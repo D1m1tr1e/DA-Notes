2086d3ea805a20fda15914d166068fe3469eb774
D1m1tr1e <126110046+D1m1tr1e@users.noreply.github.com>
2024-02-17 13:52:59 +0100

    .

--- DA-Notes/src/app/firebase-services/note-list.service.ts
+++ DA-Notes/src/app/firebase-services/note-list.service.ts
@@ -1,10 +1,42 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Note } from '../interfaces/note.interface'
+import { Firestore, collectionData, doc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { collection } from '@firebase/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteListService {
 
-  constructor() { }
+  firestore: Firestore = inject(Firestore);
+  items$;
+  items;
+  //const itemCollection = collection(this.firestore, 'item');
+
+  //Greit auf die Sammlung Notes aus dem Projekt /Firebase
+  getNotesRef() { 
+    return collection(this.firestore, 'notes');
+  }
+
+  //Greift auf den Sammlung Trash im Projekt aus Firebase 
+  getTrashRef() {
+    return collection(this.firestore, 'trash');
+  }
+
+  //Greift auf den Sammlung der IDs im Projekt aus Firebase
+  getSingleDocRef(colId: string, docId: string) { 
+    return doc(collection(this.firestore, colId, docId));
+  }
+
+  constructor() {
+    this.items$ = collectionData(this.getNotesRef())
+    this.items = this.items$.subscribe((list) =>{
+      list.forEach(element => {
+        console.log(element);
+      });
+    } );
+    this.items.unsubscribe();
+  }
+  
 }
